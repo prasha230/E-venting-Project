16d11e8e3d13cf5d61ee602fd6d7debbf9247c40
Prashant Rai <prashantrai230@gmail.com>
2022-07-10 00:35:37 +0530

    whitenoise added

--- E-venting/settings.py
+++ E-venting/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'E-venting.urls'
@@ -159,6 +160,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'events\static'),)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'events\static')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
