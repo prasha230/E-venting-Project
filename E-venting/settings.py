e966238da74373363f34dd255fdbe0c24e878ea2
Prashant Rai <prashantrai230@gmail.com>
2022-07-09 23:24:13 +0530

    setting.py changed

--- E-venting/settings.py
+++ E-venting/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('django-insecure-gnsw5x!*29fac5d5f+w=@+uw2#*_g$moyr@+$kpt4usfrrw2+6')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
